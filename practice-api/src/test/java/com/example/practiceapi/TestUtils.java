e54801b6fccd561b01b670b767a57813a26e3951
Martín Braga <martin.braga252@gmail.com>
2023-09-06 09:17:32 -0300

    Created Test Utils
    About to make the corresponding unit tests

---
+++ practice-api/src/test/java/com/example/practiceapi/TestUtils.java
@@ -0,0 +1,17 @@
+package com.example.practiceapi;
+
+import com.example.practiceapi.models.de.ProductDE;
+
+public class TestUtils {
+
+//    public static ProductDE getProductDE(){
+//        return ProductDE.builder()
+//                .id(1L)
+//                .name("product")
+//                .price("$1234")
+//                .stock("45")
+//                .productCode("iL692")
+//                .productCategories()
+//                .build()
+//    }
+}
